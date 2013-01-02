79a1764c35a6f4f22f6d8e84d7fbd13f450fe8c1
Travis Mosley <tprowx@gmail.com>
2013-01-01 22:16:40 -0500

    first commit

---
+++ SimpleApp/src/org/drchaos/SimpleApp.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.drchaos;
+
+/**
+ *
+ * @author drchaos
+ */
+public class SimpleApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
